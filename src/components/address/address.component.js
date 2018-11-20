d6597bff03cf9ce8d38a17d7edbe7a495540972b
Bull Bear Team <bullbearnetwork@gmail.com>
2018-11-20 20:14:07 +0100

    fix ellipsis and regex

--- src/components/address/address.component.js
+++ src/components/address/address.component.js
@@ -94,7 +94,7 @@ const AddressConstructor = function ($rootScope, $stateParams, $location, $http,
 		vm.txs.loadData();
 	};
 
-	const isValidAddress = id => /[a-zA-Z0-9]{32,33}BB(T|N)$/.test(id);
+	const isValidAddress = id => /^[a-zA-Z0-9]{32,33}BB(T|N)$/.test(id);
 
 	$rootScope.$on('advanced-searchbox:modelUpdated', (event, model) => {
 		const params = {};
